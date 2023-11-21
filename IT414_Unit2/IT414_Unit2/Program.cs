ff512d4d1003a3f5ece3023be046b7076447221e
Stephen VandenOever <151676015+StephenVandenOever-Student@users.noreply.github.com>
2023-11-21 17:03:56 -0500

    Add files via upload

---
+++ IT414_Unit2/IT414_Unit2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IT414_Unit2
+{
+    public class Unit_2
+    {
+        public static void Main(string[] args)
+        {
+            int Number1, Number2;
+            Console.WriteLine("What is the First Number?");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the second number?");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+            int Result;
+            Result = Number1 + Number2;
+            Console.WriteLine("The sum of the two numbers is: " + Result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IT414_Unit2/IT414_Unit2_Tests/Unit_2_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IT414_Unit2;
+
+namespace IT414_Unit2_Tests
+{
+    public class Unit_2_Tests
+    {
+        [Xunit.Fact]
+        public void Unit_2_Tests()
+        {
+            // Arrange
+            double expected = 10;
+
+            //Act
+            double actual = result.Equals(5, 5);
+
+            //Assert
+            Xunit.Assert.Equal(expected, actual);
+        }
+    }
+}
